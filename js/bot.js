9044b5057c4ad8e57b1c35496d52043d8539dbbc
yuyik <st.pokemon@hotmail.com>
2021-05-24 20:33:31 +0700

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -3,14 +3,12 @@ class bot{
   constructor() {
     this.isBotRunning = false;
     this.alertCaptcha = false;
-    this.checkCpuPercent = 90;
+    this.checkCpuPercent = 80;
     this.timerDelay = 810000;
-    this.timerDelayCpu = 180000;
+	this.timerDelayCpu = 180000;
     this.checkMinedelay = false;
     this.firstMine = true;
     this.previousMineDone = false;
-    this.lineToken = '';
-    this.lineBypassUrl = 'https://notify-gateway.vercel.app/api/notify';
 }
 
 delay = (millis) =>
@@ -18,45 +16,44 @@ delay = (millis) =>
     setTimeout((_) => resolve(), millis);
   });
 
-isEmptyObject(obj) {
-  return Object.keys(obj).length === 0 && obj.constructor === Object;
-}
-
-async postData(url = '', data = {}, method = 'POST',header = {'Content-Type': 'application/json'}) {
-  try {
-    const init = (method == 'POST') ? {method: method,mode: 'cors', cache: 'no-cache',credentials: 'same-origin',headers: header,redirect: 'follow',referrerPolicy: 'no-referrer',body: JSON.stringify(data)} : {method: method,mode: 'cors', cache: 'no-cache',credentials: 'same-origin',headers: header,redirect: 'follow',referrerPolicy: 'no-referrer'}
-    const response = await fetch(url, init);
-
-    return response.json(); // parses JSON response into native JavaScript objects
-  }catch (err) {
-    this.appendMessage(`Error:${err.message}`)
-    //send bypass line notify
-    if(this.lineToken !== ''){
-      await this.postData(this.lineBypassUrl, { token: this.lineToken, message:`Fetch:error, User:${userAccount}, Message:${err.message}` })
-    }
-    return false;
-  }
+async postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
 
 async checkCPU (userAccount){
-  let result = true
-  let i = 0;
-  let accountDetail = {}
+  let result = true;
   while(result){
-    if(i%2 > 0){
-      accountDetail = await this.postData('https://wax.cryptolions.io/v2/state/get_account?account='+userAccount, {}, 'GET')
-      accountDetail = accountDetail.account;
-    }else{
-      accountDetail = await this.postData('https://api.waxsweden.org/v1/chain/get_account', { account_name: userAccount })
-    }
-      console.log('accountDetail',accountDetail)
-    if(accountDetail){
-      const rawPercent = ((accountDetail.cpu_limit.used/accountDetail.cpu_limit.max)*100).toFixed(2)
-      console.log(`%c[Bot] rawPercent : ${rawPercent}%`, 'color:green')
-      this.appendMessage(`CPU ${rawPercent}%`)
-      if(rawPercent < this.checkCpuPercent){
-        result = false;
+    try {
+      const accountDetail = await this.postData('https://api.waxsweden.org/v1/chain/get_account', { account_name: userAccount })
+      
+      if(accountDetail.cpu_limit != null){
+        const rawPercent = ((accountDetail.cpu_limit.used/accountDetail.cpu_limit.max)*100).toFixed(2)
+        console.log(`%c[Bot] Raw CPU Percent : ${rawPercent}%`, 'color:yellow')
+        this.appendMessage(`CPU ${rawPercent}%`)
+        if(rawPercent < this.checkCpuPercent){
+          result = false;
+        }
       }
+    }catch (err) {
+      console.log(err.message);
+	  this.appendMessage(`Error:${err.message}`)
+	  this.appendMessage(`Error:${err.message}`)
+	  
+      result = false;
     }
     
     if(result){
@@ -64,21 +61,21 @@ async checkCPU (userAccount){
       const delayCheckCpu = this.timerDelayCpu
       this.appendMessage(`CPU delay check ${Math.ceil(delayCheckCpu/1000/60)} min`)
       await this.delay(delayCheckCpu + randomTimer);
-      i ++;
     }
   }
 }
 
 appendMessage(msg , box = ''){
-  const dateNow = moment().format('DD/MM/YYYY h:mm:ss');
+  const dateNow = moment().format('  hh:mm ');
   const boxMessage = document.getElementById("box-message"+box)
   boxMessage.value += '\n'+ `${dateNow} : ${msg}`
   boxMessage.scrollTop = boxMessage.scrollHeight;
 }
 
 countDown(countDown){
-  let countDownDisplay = countDown/1000;
-  var x = setInterval(function() {
+  var countDownDisplay = (parseFloat(countDown/1000)).toFixed(2);
+  
+  const x = setInterval(function() {
     document.getElementById("text-cooldown").innerHTML = countDownDisplay + " Sec"
     countDown = countDown - 1000;
     countDownDisplay = countDown/1000;
@@ -96,7 +93,7 @@ async getUserMineDelay(userAccount){
 async stop() {
   this.isBotRunning = false;
   this.appendMessage("bot STOP")
-  console.log(`%c[Bot] stop`, 'color:green');
+  console.log(`%c[Bot] stop`, 'color:yellow');
 }
 
 async start() {
@@ -122,12 +119,14 @@ async start() {
       }else{
         minedelay = await getMineDelay(userAccount);
       }
-      // console.log(`%c[Bot] Cooldown for ${Math.ceil((minedelay / 1000)/60)} min`, 'color:green');
+      // console.log(`%c[Bot] Cooldown for ${Math.ceil((minedelay / 1000)/60)} min`, 'color:yellow');
       this.countDown(minedelay)
       const RandomTimeWait = minedelay + Math.floor(1000 + (Math.random() * 9000))
       this.appendMessage(`Cooldown for ${Math.ceil((RandomTimeWait / 1000)/60)} min`)
       await this.delay(RandomTimeWait);
-      minedelay = 0;      
+      minedelay = 0;
+      console.log("bot checkCPU1");
+      await this.checkCPU(userAccount);
     } while (minedelay !== 0 && (this.previousMineDone || this.firstMine));
     await this.mine(userAccount)
   }
@@ -136,8 +135,7 @@ async start() {
 async mine(userAccount){
   document.getElementById("btn-mine").disabled = true
   const balance = await getBalance(userAccount, wax.api.rpc);
-    // console.log(`%c[Bot] balance: (before mine) ${balance}`, 'color:green');
-    document.getElementById("text-balance").innerHTML = balance
+    console.log(`%c[Bot] balance: (before mine) ${balance}`, 'color:yellow');
     
     const mine_work = await background_mine(userAccount);
     unityInstance.SendMessage(
@@ -181,13 +179,13 @@ async mine(userAccount){
           expireSeconds: 360,
         }
       );
-      console.log(`%c[Bot] result is = ${result}`, 'color:green');
+      console.log(`%c[Bot] result is = ${result}`, 'color:yellow');
       var amounts = new Map();
       if (result && result.processed) {
         result.processed.action_traces[0].inline_traces.forEach((t) => {
           if (t.act.data.quantity) {
             const mine_amount = t.act.data.quantity;
-            console.log(`%c[Bot] ${mine_work.account} Mined ${mine_amount}`, 'color:green');
+            console.log(`%c[Bot] ${mine_work.account} Mined ${mine_amount}`, 'color:yellow');
             if (amounts.has(t.act.data.to)) {
               let obStr = amounts.get(t.act.data.to);
               obStr = obStr.substring(0, obStr.length - 4);
@@ -220,16 +218,12 @@ async mine(userAccount){
       this.checkMinedelay = false;
       console.log(`%c[Bot] Error:${err.message}`, 'color:red');
       this.appendMessage(`Error:${err.message}`)
-      //send bypass line notify
-      if(this.lineToken !== ''){
-        await this.postData(this.lineBypassUrl, { token: this.lineToken, message:`User:${userAccount} , Message:${err.message}` })
-      }
     }
 
     const afterMindedBalance = await getBalance(userAccount, wax.api.rpc);
     this.appendMessage(`balance (after mined): ${afterMindedBalance}`)
     document.getElementById("text-balance").innerHTML = afterMindedBalance
-    // console.log(`%c[Bot] balance (after mined): ${afterMindedBalance}`, 'color:green');
+    // console.log(`%c[Bot] balance (after mined): ${afterMindedBalance}`, 'color:yellow');
     document.getElementById("btn-mine").disabled = false
 }
 
