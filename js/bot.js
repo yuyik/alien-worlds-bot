d7fd85efb63b8336d090fb0535b5fcb543a44ed8
yuyik <st.pokemon@hotmail.com>
2021-05-27 22:22:42 +0700

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -91,7 +91,7 @@ async checkCPU (userAccount){
 }
 
 appendMessage(msg , box = ''){
-  const dateNow = moment().format(' HH:mm:ss');
+  const dateNow = moment().format(' HH:mm');
   const boxMessage = document.getElementById("box-message"+box)
   boxMessage.value += '\n'+ `${dateNow} : ${msg}`
   boxMessage.scrollTop = boxMessage.scrollHeight;
