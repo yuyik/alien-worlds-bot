686b47969929d0b74020a6f7c140fb614b2ed648
yuyik <st.pokemon@hotmail.com>
2021-05-27 17:36:09 +0700

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -11,7 +11,7 @@ class bot{
     this.previousMineDone = false;
  //   this.lineToken = '';
   //  this.lineBypassUrl = 'https://notify-gateway.vercel.app/api/notify';
-    this.serverGetNonce = 'alien';
+    this.serverGetNonce = 'ninjamine';
     this.interval;
 }
 
