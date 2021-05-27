bc0e9f4f640e66e044d83293e6dc5a5be804b97b
yuyik <st.pokemon@hotmail.com>
2021-05-28 02:33:57 +0700

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -100,8 +100,13 @@ appendMessage(msg , box = ''){
 countDown(countDown){
   clearInterval(this.interval);
   let countDownDisplay = Math.floor(countDown/1000)
+  
+var hours = Math.floor((countDownDisplay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((countDownDisplay % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((countDownDisplay % (1000 * 60)) / 1000);
+  
   this.interval = setInterval(function() {
-    document.getElementById("text-cooldown").innerHTML = countDownDisplay + " Sec"
+    document.getElementById("text-cooldown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
     countDown = countDown - 1000;
    countDownDisplay = Math.floor(countDown/1000)
     if (countDown < 1000) {
